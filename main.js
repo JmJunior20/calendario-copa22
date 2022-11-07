348f6ac410b7e40065fe6d8e9b285a3d0037a661
JmJunior20 <jmjuniormonteiro@gmail.com>
2022-11-07 20:56:30 -0300

    Refatorando o código

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function createGame(player1, hour, player2) {
-    return `
+  return `
         <li>
             <img src="./assets/${player1}.svg" alt="Bandeira do ${player1}" />
             <strong>${hour}</strong>
             <img src="./assets/${player2}.svg" alt="Bandeira do ${player2}" />
         </li>
-    `
+    `;
 }
 
 function createCard(date, day, games) {
@@ -19,26 +19,20 @@ function createCard(date, day, games) {
     `;
 }
 
-document.querySelector("#app").innerHTML = `
-    <header>
-        <h1>COPA DO MUNDO 22</h1>
-        <h2>CALENDÁRIO</h2>
-    </header>
-
-    <main id="cards">
-        ${createCard('20/11', 'domingo', 
-        createGame('Catar', '13:00', 'Equador')
-        )}
-        ${createCard('21/11', 'segunda', 
-        createGame('Inglaterra', '10:00', 'Ira') + 
-        createGame('Senegal', '13:00', 'Holanda') + 
-        createGame('Estados_Unidos', '16:00', 'Pais_de_Gales')
-        )}
-        ${createCard('22/11', 'terça', 
-        createGame('Argentina', '07:00', 'Arabia_Saudita') + 
-        createGame('Dinamarca', '10:00', 'Tunisia') + 
-        createGame('Mexico', '13:00', 'Polonia') + 
-        createGame('Franca', '16:00', 'Australia')
-        )}
-    </main>
-`;
+document.querySelector("#cards").innerHTML =
+  createCard("20/11", "domingo", createGame("Catar", "13:00", "Equador")) +
+  createCard(
+    "21/11",
+    "segunda",
+    createGame("Inglaterra", "10:00", "Ira") +
+      createGame("Senegal", "13:00", "Holanda") +
+      createGame("Estados_Unidos", "16:00", "Pais_de_Gales")
+  ) +
+  createCard(
+    "22/11",
+    "terça",
+    createGame("Argentina", "07:00", "Arabia_Saudita") +
+      createGame("Dinamarca", "10:00", "Tunisia") +
+      createGame("Mexico", "13:00", "Polonia") +
+      createGame("Franca", "16:00", "Australia")
+  );
