9963b5ef2a227a950ab0d7ec373bb91079588ac2
JmJunior20 <jmjuniormonteiro@gmail.com>
2022-11-07 20:26:19 -0300

    Criando o javaScript

---
+++ main.js
@@ -0,0 +1,20 @@
+document.querySelector("#app").innerHTML = `
+    <header>
+        <h1>COPA DO MUNDO 22</h1>
+        <h2>CALENDÁRIO</h2>
+    </header>
+
+    <main id="cards">
+        <div class="card">
+            <h2>22/11 <span>domingo</span></h2>
+
+            <ul>
+            <li>
+                <img src="./assets/Catar.svg" alt="Bandeira do Catar" />
+                <strong>13:00</strong>
+                <img src="./assets/Equador.svg" alt="Bandeira do Equador" />
+            </li>
+            </ul>
+        </div>
+    </main>
+`;
